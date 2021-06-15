131adfa15bb3df0a72121c27283b172ece16cb7b
SSGSSACE <quangbayhai@gmail.com>
2021-06-15 23:42:26 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class Person
+{
+private:
+    static int ID;
+    int age;
+    int pID;
+    string name;
+    string address;
+    string phone;
+    string gender;
+
+public:
+
+};
+int main()
+{
+    
+    return 0;
+}
